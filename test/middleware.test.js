fc0ac71361631e814aa22e2099e7f70d058c4b1f
blackcathacker <douglas.pace@gmail.com>
2018-07-26 18:11:26 -0700

    Additional config options to customize middleware logger

--- test/middleware.test.js
+++ test/middleware.test.js
@@ -14,22 +14,26 @@ const stream = {
 const obscureHeaders = ['obscure']
 const excludeHeaders = ['exclude']
 
-const log = require('../lib')({
-  name: 'testLogger',
-  team: 'testTeam',
-  product: 'testProduct',
-  environment: 'testEnvironment',
-  stream,
-  obscureHeaders,
-  excludeHeaders
-})
+function createApp (customConfig) {
+  const log = require('../lib')(Object.assign({
+    name: 'testLogger',
+    team: 'testTeam',
+    product: 'testProduct',
+    environment: 'testEnvironment',
+    stream,
+    obscureHeaders,
+    excludeHeaders
+  }, customConfig))
 
-const app = express()
-app.use(log.middleware)
-app.get('/', (req, res) => {
-  req.log.info()
-  res.send('ok')
-})
+  const app = express()
+  app.use(log.middleware)
+  app.get('/', (req, res) => {
+    req.appSpecificReq = 'test'
+    req.log.info()
+    res.send('ok')
+  })
+  return app
+}
 
 beforeEach(() => {
   catcher.clear()
@@ -38,6 +42,7 @@ beforeEach(() => {
 describe('middleware', () => {
   describe('requestId', () => {
     test('sets requestId correctly if no existing requestId', done => {
+      const app = createApp()
       request(app).get('/').end((err, res) => {
         if (err) throw err
         expect(res.headers.hasOwnProperty('x-request-id')).toBe(true)
@@ -47,6 +52,7 @@ describe('middleware', () => {
     })
 
     test('leaves existing requestId unchanged', done => {
+      const app = createApp()
       request(app).get('/').set('X-Request-Id', 'test').end((err, res) => {
         if (err) throw err
         expect(res.headers['x-request-id']).toBe('test')
@@ -57,6 +63,7 @@ describe('middleware', () => {
   })
 
   describe('additional fields', () => {
+    const app = createApp()
     test('adds additional fields to log output', done => {
       request(app)
         .get('/')
@@ -70,10 +77,24 @@ describe('middleware', () => {
           done()
         })
     })
+    test('adds app specific fields', done => {
+      const app = createApp({additionalRequestFinishData: req => ({appSpecificReq: req.appSpecificReq})})
+      request(app)
+        .get('/')
+        .set('x-kuali-tenant', 'tenant')
+        .set('x-kuali-lane', 'lane')
+        .end((err, res) => {
+          if (err) throw err
+          expect(catcher.last.event).toBe('REQUEST')
+          expect(catcher.last.appSpecificReq).toBe('test')
+          done()
+        })
+    })
   })
 
   describe('obscureHeaders and excludeHeaders', () => {
     test('obscures and excludes configured headers', done => {
+      const app = createApp()
       request(app)
         .get('/')
         .set('obscure', 'test')
@@ -86,6 +107,19 @@ describe('middleware', () => {
             false
           )
           expect(catcher.last.req.headers.authorization).toBeNull()
+          done()
+        })
+    })
+  })
+
+  describe('filters requests', () => {
+    test('filters requests based on filter function', done => {
+      const app = createApp({middlewareFilter: req => req.url.includes('filtered')})
+      request(app)
+        .get('/filtered')
+        .end((err, res) => {
+          if (err) throw err
+          expect(catcher.last).toBeFalsy()
           done()
         })
     })
